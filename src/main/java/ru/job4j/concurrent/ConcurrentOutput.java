c53027c64b0299c607e569769f74432773cad38e
MikhailKuliev <mkh_3112@mail.ru>
2025-06-09 02:17:19 +0300

    thread 1

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        // первый поток с автомат именем
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();// второй поток second
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()),
+                "second"
+        );
+    second.start();
+        // главный поток
+        System.out.println(Thread.currentThread().getName());
+
+    }
+}
+
+
+
+
+
