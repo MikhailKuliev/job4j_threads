8d196bb97fe34dfef7ff9ed37b7382bd5727b4eb
MikhailKuliev <mkh_3112@mail.ru>
2025-06-10 04:02:10 +0300

    нити

--- src/main/java/org/example/ConcurrentOutput.java
+++ src/main/java/org/example/ConcurrentOutput.java
@@ -1,4 +1,4 @@
-package ru.job4j.concurrent;
+package org.example;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
--- src/main/java/org/example/ThreadState.java
+++ src/main/java/org/example/ThreadState.java
@@ -1,4 +1,4 @@
-package ru.job4j.concurrent;
+package org.example;
 
 public class ThreadState {
     public static void main(String[] args) {
