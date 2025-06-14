f38759c5278de477559072d56e6084238c311f8b
MikhailKuliev <mkh_3112@mail.ru>
2025-06-14 04:22:07 +0300

    нити

---
+++ src/job4j/ThreadState.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()),
+                "first"//1 нить
+
+
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()),
+
+                "second"  // 2 нить
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED) { //ЦИКЛ ПРОВЕРКИ
+            System.out.println(first.getState());
+        } while (second.getState() != Thread.State.TERMINATED) { //ЦИКЛ ПРОВЕРКИ
+            System.out.println(second.getState());
+        }
+
+
+        System.out.println(first.getState());
+
+        System.out.println(second.getState());//ВЫВЕДЕТ TER НИТЬ ЗАВЕРШЕНА
+    }
+}
