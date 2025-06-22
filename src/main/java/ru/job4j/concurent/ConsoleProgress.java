a78256a343a6888078afb9be854cd47fd375cb93
MikhailKuliev <mkh_3112@mail.ru>
2025-06-23 02:41:23 +0300

    Thread stop

---
+++ src/main/java/ru/job4j/concurent/ConsoleProgress.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurent;
+public class ConsoleProgress {
+    public static void main(String[] args) throws InterruptedException {
+        Thread ThreadStop = new Thread(() ->{
+            char[] process = new char[]{'-', '\\', '|', '/'};
+            int count = 0;
+            try{
+                while(!Thread.currentThread().isInterrupted()){
+                    System.out.print("\rLoading..." + process[count]);
+                    count = (count + 1)%process.length;
+
+                    Thread.sleep(500);
+                }
+            }catch(InterruptedException e){
+                Thread.currentThread().interrupt();
+                System.out.print("\rЗагрузка Завершена! ");
+            }
+        });
+        ThreadStop.start();
+        Thread.sleep(5000);
+        ThreadStop.interrupt();
+
+        }
+    }
