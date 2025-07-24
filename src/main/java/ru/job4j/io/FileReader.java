953763e1bd1968ac34f04878b3f843e2b41ddc31
MikhailKuliev <mkh_3112@mail.ru>
2025-07-24 03:02:08 +0300

    1

--- src/main/java/ru/job4j/io/FileReader.java
+++ src/main/java/ru/job4j/io/FileReader.java
@@ -7,19 +7,15 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.function.Predicate;
 
-public final class ParseFile {
+public final class FileReader {
     private final File file;
-
-    public ParseFile(File file) {
+    public FileReader(File file) {
         this.file = file;
-
     }
-
     public String content(Predicate<Character> filter) throws IOException {
         StringBuilder output = new StringBuilder();
         try (InputStream input = new BufferedInputStream(new FileInputStream(file));
              Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8)) {
-
             int data;
             while ((data = reader.read()) !=-1) {
 
@@ -27,8 +23,6 @@ public final class ParseFile {
                 if (filter.test(c)) {
                     output.append(c);
                 }
-
-                output.toString();
             }
         }
         return output.toString();
@@ -36,31 +30,21 @@ public final class ParseFile {
     public String GetContent() throws IOException {
         return new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
     }
-    public String getContentWithoutUnicode() throws IOException {
+    public String GetContentWithoutUnicode() throws IOException {
 
         return content(c ->c<0x80);
-
-
-
     }
-    public final class WriterFile{
+    public static class FileWriter{
         private final File file;
-        public WriterFile(File file) {
+        public FileWriter(File file) {
             this.file = file;
 
         }
         public void saveContent(String content) throws IOException {
-
             try (OutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
                 Writer w = new OutputStreamWriter(o, StandardCharsets.UTF_8 );
                 w.write(content);
-
-
-
             }
-
-
         }
-
     }
 }
