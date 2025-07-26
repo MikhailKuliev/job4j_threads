a0eef94ab4fde4c44b2ead0e3a501aa3737326f1
MikhailKuliev <mkh_3112@mail.ru>
2025-07-26 03:14:28 +0300

    1

--- src/job4j/io/FileReader.java
+++ src/job4j/io/FileReader.java
@@ -1,1 +1,42 @@
+package ru.job4j.io;
 
+
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Predicate;
+
+public final class FileReader {
+    private final File file;
+    public FileReader(File file) {
+        this.file = file;
+    }
+    public String content(Predicate<Character> filter) throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (InputStream input = new BufferedInputStream(new FileInputStream(file));
+             Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8)) {
+            int data;
+            while ((data = reader.read()) !=-1) {
+
+                char c = (char) data;
+                if (filter.test(c)) {
+                    output.append(c);
+                }
+            }
+        }
+        return output.toString();
+    }
+    public static final class FileWriter{
+        private final File file;
+        public FileWriter(File file) {
+            this.file = file;
+
+        }
+        public void saveContent(String content) throws IOException {
+            try (OutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+                Writer w = new OutputStreamWriter(o, StandardCharsets.UTF_8 );
+                w.write(content);
+            }
+        }
+    }
+}
