1a359d71ba896bbaf6ed52eaa4fed3b46330b0bb
MikhailKuliev <mkh_3112@mail.ru>
2025-08-18 11:20:50 +0300

    producer consumer

---
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -0,0 +1,52 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int capacity;
+
+    public SimpleBlockingQueue() {
+        this(10);
+    }
+
+    public SimpleBlockingQueue(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.capacity = capacity;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        synchronized (this) {
+            while (queue.size() >= capacity) {
+                wait();
+            }
+            queue.offer(value);
+            notifyAll();
+        }
+    }
+
+    public T poll() throws InterruptedException {
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                wait();
+            }
+            T value = queue.poll();
+            notifyAll();
+
+            return value;
+        }
+    }
+
+    public synchronized int size() {
+        return queue.size();
+    }
+}
