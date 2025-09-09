d699ea6e80e1d24f4056578df9073e820b2a9180
MikhailKuliev <mkh_3112@mail.ru>
2025-09-09 03:44:57 +0300

    Cache

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,76 @@
+package ru.job4j.cache;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) throws OptimisticException {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        while (true) {
+            Base stored = memory.get(model.getId());
+            if (stored == null) {
+                throw new OptimisticException("Model not found");
+            }
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            Base updated = new Base(model.getId(), model.getName(), stored.getVersion() + 1);
+            if (memory.replace(model.getId(), stored, updated)) {
+                return true;
+            }
+        }
+    }
+
+    public void delete(int id) {
+        memory.remove(id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
+
+class Base {
+    private final int id;
+    private final String name;
+    private final int version;
+
+    public Base(int id, String name) {
+        this(id, name, 0);
+
+    }
+
+    public Base(int id, String name, int version) {
+        this.id = id;
+        this.name = name;
+        this.version = version;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+}
+
+class OptimisticException extends RuntimeException {
+    public OptimisticException(String message) {
+        super(message);
+    }
+}
